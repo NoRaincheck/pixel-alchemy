ed1f81d6ce8ccb735637140bb4653b21189eadb7
NoRaincheck <2498638+NoRaincheck@users.noreply.github.com>
2026-09-07 08:20:57 +1000

    bump

--- src/pixel_alchemy/incidental/reference/ambient_darin.rb
+++ src/pixel_alchemy/incidental/reference/ambient_darin.rb
@@ -1,11 +1,3 @@
-# Coded by Darin Wilson
-#
-# The piece consists of three long loops, each of which
-# plays one of two randomly selected pitches. Each note
-# has different attack, release and sleep values, so that
-# they move in and out of phase with each other. This can
-# play for quite awhile without repeating itself :)
-
 use_synth :hollow
 with_fx :reverb, mix: 0.7 do
 
